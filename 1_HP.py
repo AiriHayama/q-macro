07f01bd488fd5c090ac10a60ababd14082c562f2
Airi Hayama <bce.hayama@gmail.com>
2025-05-19 12:00:57 +0900

    いけた

--- 1_HP.py
+++ 1_HP.py
@@ -14,14 +14,21 @@ gdp = web.DataReader('RGDPNAQAA666NRUG', 'fred', start_date, end_date)
 log_gdp = np.log(gdp)
 
 # apply a Hodrick-Prescott filter to the data to extract the cyclical component
-cycle, trend = sm.tsa.filters.hpfilter(log_gdp, lamb=1600)
+cycle, trend1600 = sm.tsa.filters.hpfilter(log_gdp, lamb=1600)
+cycle, trend100 = sm.tsa.filters.hpfilter(log_gdp, lamb=100)
+cycle, trend10 = sm.tsa.filters.hpfilter(log_gdp, lamb=10)
 
 # Plot the original time series data
 plt.plot(log_gdp, label="Original GDP (in log)")
 
 # Plot the trend component
-plt.plot(trend, label="Trend")
+plt.plot(trend1600, label="$\lambda$=1600")
+plt.plot(trend100, label="$\lambda$=100")
+plt.plot(trend10, label="$\lambda$=10")
 
+
+plt.legend()
+plt.title('Qatar')
 # Add a legend and show the plot
 plt.legend()
 plt.savefig('gdp_trend_qatar.pdf')
